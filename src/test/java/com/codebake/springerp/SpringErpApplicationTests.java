c6e183044835729d7193ab9feae7c8a30f0e7f81
Bereket Assefa <bereketsiyum@gmail.com>
2025-07-27 12:12:55 +0900

    bootstrap an a springboot project

---
+++ src/test/java/com/codebake/springerp/SpringErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebake.springerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
